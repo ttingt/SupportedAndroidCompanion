bfdfdd828ab9af0ca375aab71eb7b0816b2ed3f5
Ting Ting Tai <tting.git@gmail.com>
2015-06-27 18:50:32 -0700

    delivery msg to pebble watch

--- app/src/main/java/com/example/hazai/supported/TextMessager.java
+++ app/src/main/java/com/example/hazai/supported/TextMessager.java
@@ -23,7 +23,10 @@ public class TextMessager extends Activity {
 
     private static final int MAX_LOCATION_TRIES = 2;
     private static final int MAX_SEND_TRIES = 15;
+    private static final int DICT_SENT_MSG_INDEX = 2; // Fixed array index for pebble watch to expect msg to be stored at in the communication's dictionary
     private static final String SENT_INTENT_STR = "sent";
+    private static final int DICT_DELIVERED_MSG_INDEX = DICT_SENT_MSG_INDEX + 1;
+    private static final String DLVR_INTENT_STR = "delivered";
 
     private static final UUID SUPPORTED_PEBBLE_APP_UUID = UUID.fromString("TO-BE-UPDATED"); // TODO: UPDATE THIS PLEASE
     private PebbleKit.PebbleDataLogReceiver mDataLogReceiver = null;
@@ -75,14 +78,19 @@ public class TextMessager extends Activity {
         while (!success && attempts < MAX_SEND_TRIES) {
             try {
                 SmsManager smsm = SmsManager.getDefault();
+
                 Intent sentIntent = new Intent(SENT_INTENT_STR);
                 PendingIntent sentPI = PendingIntent.getBroadcast(getApplicationContext(),0,sentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                Intent dlvrIntent = new Intent(DLVR_INTENT_STR);
+                PendingIntent deliveredPI = PendingIntent.getBroadcast(getApplicationContext(),0,dlvrIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
                 if (address != null) {
                     address = "me. My location could not be determined at this time";
                 }
-                smsm.sendTextMessage(phoneNumber, null, HELPMESSAGE + address, sentPI, null);
+                smsm.sendTextMessage(phoneNumber, null, HELPMESSAGE + address, sentPI, deliveredPI);
                 success = true;
                 smsSentBroadcastReceiver();
+                smsDeliveredBroadcastReceiver();
             } catch (Exception e) {
                 attempts++;
             }
@@ -113,6 +121,18 @@ public class TextMessager extends Activity {
         }, new IntentFilter(SENT_INTENT_STR));
     }
 
+    // SMS delivery status (pending intent) broadcast receiver
+    // Receives message notifying whether sms was delivered successfully or not
+    private void smsDeliveredBroadcastReceiver() {
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                result = true;
+                alertPebbleSMSDelivered(result);
+            }
+        }, new IntentFilter(DLVR_INTENT_STR));
+    }
+
     // To be called once SMS to emergency contacts sent successfully.
     // Alerts Pebble Watch App that sms were sent successfully.
     private void alertPebbleSMSSent(boolean smsSentSuccess) {
@@ -123,6 +143,19 @@ public class TextMessager extends Activity {
         }
         PebbleDictionary data = new PebbleDictionary();
         data.addString(2, msg);
+        PebbleKit.sendDataToPebble(getApplicationContext(), SUPPORTED_PEBBLE_APP_UUID, data);
+    }
+
+    // To be called once SMS delivered to emergency contacts
+    // Alerts Pebble Watch App that sms were delivered successfully.
+    private void alertPebbleSMSDelivered(boolean smsDeliverySuccess) {
+        // TODO: implement
+        String msg = "false";
+        if (smsSentSuccess) {
+            msg = "true";
+        }
+        PebbleDictionary data = new PebbleDictionary();
+        data.addString(3, msg);
         PebbleKit.sendDataToPebble(getApplicationContext(), SUPPORTED_PEBBLE_APP_UUID, data);
     }
 
